5877684c5c2253e5dacf474d493a7717dcd9fe4f
Maxim Lakhman <107705528+lakhmanmaxim@users.noreply.github.com>
2023-02-19 17:24:47 +0200

    add local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,6 +15,20 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+    if (contacts?.length) {
+      this.setState({ contacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+    if (prevState.contacts.length !== contacts.length) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
+
   onInputChange = ({ target }) => {
     const { name, value } = target;
     this.setState({
